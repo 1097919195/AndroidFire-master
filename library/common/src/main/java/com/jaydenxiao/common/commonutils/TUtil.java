066a95c6f0510ac0f8eaf1a926841cffcb68bca1
zjl7577 <1097919195@qq.com>
2018-09-07 11:35:12 +0800

    类转化

--- library/common/src/main/java/com/jaydenxiao/common/commonutils/TUtil.java
+++ library/common/src/main/java/com/jaydenxiao/common/commonutils/TUtil.java
@@ -6,6 +6,10 @@ import java.lang.reflect.ParameterizedType;
  * 类转换初始化
  */
 public class TUtil {
+    //(class)((ParameterizedType)(this.getClass(）.getGenericSuperclass()).getActualTypeArguments()[0] 这是一个获取类的方法
+    //ParameterizedType实现泛型类类型参数化
+    //getGenericSuperclass方法可以获取当前对象的直接超类的 Type
+    //getActualTypeArguments 得到该泛型
     public static <T> T getT(Object o, int i) {
         try {
             return ((Class<T>) ((ParameterizedType) (o.getClass()
